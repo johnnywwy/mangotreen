dd185c233996f30b661a05c5d44637d3137103ce
文宇 <13682265212@163.com>
2023-02-18 17:29:25 +0800

    完善+1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     {{ count }}
-    <button @click="onClick"></button>
   </div>
+  <button @click="onClick">+1</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const count = ref();
+const count = ref(1);
 const onClick = () => {
   count.value += 1;
 };
