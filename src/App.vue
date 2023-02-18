3703c7b0281c8af22d1d0531b83189649d312d76
文宇 <13682265212@163.com>
2023-02-18 17:26:51 +0800

    加1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>HI</div>
+  <div>
+    {{ count }}
+    <button @click="onClick"></button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref();
+const onClick = () => {
+  count.value += 1;
+};
+</script>
+
+<!-- <style lang="" scoped></style> -->
