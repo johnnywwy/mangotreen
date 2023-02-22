1321b7e02bbcb13904953c5597cb85aa1a436c3b
文宇 <13682265212@163.com>
2023-02-22 14:33:21 +0800

    安装sass 使用css module

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,14 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import s from "./Welcome.module.scss";
 export const welcome = defineComponent({
   setup() {
     return () => (
       <>
-        <div>
+        <div class={s.wrapper}>
+          <header>logo</header>
           <RouterView />
+          <footer>按钮</footer>
         </div>
       </>
     );
