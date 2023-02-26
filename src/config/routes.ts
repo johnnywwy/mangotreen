308d74647823ff0e9d60503631ca01096d3cfb82
johnnywwy <johnnywwy@gmail.com>
2023-02-26 21:29:23 +0800

    修改默认路由，删除无用页面

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,7 +1,5 @@
 import { RouteRecordRaw } from "vue-router";
 
-import { Foo } from "../views/Foo";
-import { Bar } from "../views/Bar";
 import { welcome } from "../views/Welcome";
 
 import { first } from "../components/welcome/first";
@@ -10,12 +8,12 @@ import { third } from "../components/welcome/third";
 import { forth } from "../components/welcome/forth";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: Foo },
-  { path: "/about", component: Bar },
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: welcome,
     children: [
+      { path: "", redirect: "/welcome/1" },
       { path: "1", component: first },
       { path: "2", component: second },
       { path: "3", component: third },
