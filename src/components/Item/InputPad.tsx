034af3568bd4f85014b7ca66587f81f2bf2b53af
johnnywwy <johnnywwy@gmail.com>
2023-06-24 21:45:38 +0800

    wip: 制作数字按钮

--- src/components/Item/InputPad.tsx
+++ src/components/Item/InputPad.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, PropType } from "vue";
 import s from "./InputPad.module.scss";
+import { Icon } from "../../shared/Icon";
 
 export const InputPad = defineComponent({
   props: {
@@ -8,6 +9,37 @@ export const InputPad = defineComponent({
     },
   },
   setup: (props, content) => {
-    return () => <div class={s.wrapper}>数字版</div>;
+    const buttons = [
+      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "删除", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "确定", onClick: () => {} },
+    ];
+    return () => (
+      <div class={s.wrapper}>
+        <div>
+          <span class={s.note}>
+            <Icon name="logo"></Icon>
+            <span>2023-06-24</span>
+          </span>
+          <span class={s.amount}>数字</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((item) => (
+            <button onClick={item.onClick}>{item.text}</button>
+          ))}
+        </div>
+      </div>
+    );
   },
 });
