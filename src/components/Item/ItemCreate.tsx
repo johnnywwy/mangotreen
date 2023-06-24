1458788d0a582a47ab04d0f74ec10ce764fbedec
johnnywwy <johnnywwy@gmail.com>
2023-06-24 21:07:23 +0800

    feat: 创建记账页面

--- src/components/Item/ItemCreate.tsx
+++ src/components/Item/ItemCreate.tsx
@@ -3,6 +3,7 @@ import s from "./ItemCreate.module.scss";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tabs, Tab } from "../../shared/Tabs";
+import { InputPad } from "./InputPad";
 export const ItemCreate = defineComponent({
   props: {
     name: {
@@ -11,7 +12,7 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, content) => {
     const selected = ref("支出");
-    const onUpdateSelected = (name: string) => (selected.value = name);
+    // const onUpdateSelected = (name: string) => (selected.value = name);
 
     return () => (
       <MainLayout>
@@ -25,11 +26,12 @@ export const ItemCreate = defineComponent({
                 // onUpdateSelected={(name) => (selected.value = name)}
                 v-model:selected={selected.value}
               >
-                <Tab name="支出">icon 列表111</Tab>
+                <Tab name="支出">icon 列表1</Tab>
                 <Tab name="收入">icon 列表2</Tab>
               </Tabs>
-              {/* <Tabs name="支出">icon 列表1</Tabs> */}
-              {/* <Tabs name="收入">icon 列表2</Tabs> */}
+              <div class={s.inputPad_wrapper}>
+                <InputPad />
+              </div>
             </>
           ),
         }}
