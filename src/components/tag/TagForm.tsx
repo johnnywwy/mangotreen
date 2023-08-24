542d60b5568fbece3761822aad39467cb5294800
wwy <johnnywwy@gmail.com>
2023-08-24 18:22:37 +0800

    重构 Tag 组件

--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,7 +1,8 @@
-import { defineComponent, PropType, reactive } from "vue";
+import { defineComponent, PropType, reactive, toRaw } from 'vue';
 import { Button } from "../../shared/Button";
-import { EmojiSelected } from "../../shared/EmojiSelected";
-import { validate } from "../../shared/validate";
+// import { EmojiSelected } from "../../shared/EmojiSelected";
+import { Form, FormItem } from "../../shared/Form";
+import { Rules, validate } from "../../shared/validate";
 import s from "./Tag.module.scss";
 export const TagForm = defineComponent({
     props: {
@@ -13,80 +14,55 @@ export const TagForm = defineComponent({
         const formData = reactive({
             name: "",
             sign: "",
-          });
-      
-          const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
-      
-          //提交表单
-          const onSubmit = (e: Event) => {
+        });
+
+        const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
+
+        //提交表单
+        const onSubmit = (e: Event) => {
             e.preventDefault();
-      
+
             const rules: Rules<typeof formData> = [
-              { key: "name", type: "required", message: "必填" },
-              {
-                key: "name",
-                type: "pattern",
-                message: "最多 1 ～ 4 个字符",
-                regExp: /^.{1,4}$/,
-              },
-              {
-                key: "sign",
-                message: "必填",
-                type: "required",
-              },
+                { key: "name", type: "required", message: "必填" },
+                {
+                    key: "name",
+                    type: "pattern",
+                    message: "最多 1 ～ 4 个字符",
+                    regExp: /^.{1,4}$/,
+                },
+                {
+                    key: "sign",
+                    message: "必填",
+                    type: "required",
+                },
             ];
             // 重置错误信息
             Object.assign(errors, {
-              name: [],
-              sign: [],
+                name: [],
+                sign: [],
             });
-      
+
             // 验证表单
             Object.assign(errors, validate(formData, rules));
             console.log(errors);
-          };
+        };
 
         return () => (
-            <form class={s.form} onSubmit={onSubmit}>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>标签名</span>
-                  <div class={s.formItem_value}>
-                    <input
-                      v-model={formData.name}
-                      class={[s.formItem, s.input, s.error]}
-                    ></input>
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span class={s.formItem_errorHint_text}>
-                      {errors["name"]?.[0]}
-                    </span>
-                  </div>
-                </label>
-              </div>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号 {formData.sign}</span>
-                  <div class={s.formItem_value}>
-                    <EmojiSelected
-                      v-model={formData.sign}
-                      class={[s.formItem, s.emojiList, s.error]}
-                    />
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span class={s.formItem_errorHint_text}>
-                      {errors["sign"]?.[0]}
-                    </span>
-                  </div>
-                </label>
-              </div>
-              <p class={s.tips}>记账时长按标签即可进行编辑</p>
-              <div class={s.formRow}>
-                <div class={s.formItem_value}>
-                  <Button class={[s.formItem, s.button]}>确定</Button>
-                </div>
-              </div>
-            </form>
+            <Form onSubmit={onSubmit}>
+                <FormItem label='标签名'
+                    type="text"
+                    v-model={formData.name}
+                    error={errors['name'] ? errors['name'][0] : '　'} />
+                <FormItem label={'符号 ' + formData.sign}
+                    type="emojiSelect" v-model={formData.sign}
+                    error={errors['sign'] ? errors['sign'][0] : '　'} />
+                <FormItem>
+                    <p class={s.tips}>记账时长按标签即可进行编辑</p>
+                </FormItem>
+                <FormItem>
+                    <Button class={[s.button]}>确定</Button>
+                </FormItem>
+            </Form>
         )
     }
 })
