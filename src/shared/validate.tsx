57ddef2de6a9f5f15ef6116c068e21e9b7f8c721
wuwenyu <johnnywwy@gmail.com>
2023-09-12 13:56:03 +0800

    feat：判断错误数组是否为空

--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -44,3 +44,15 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
   });
   return errors;
 };
+
+// 判断Error是否为空
+export function hasError(errors: Record<string, string[]>) {
+  let result = false
+  for (let key in errors) {
+    if (errors[key].length > 0) {
+      result = true
+      break
+    }
+  }
+  return result
+}
