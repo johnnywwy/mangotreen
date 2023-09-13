78088c17cf55d5f7902c1b4b343bfe7e1e9544be
wuwenyu <johnnywwy@gmail.com>
2023-09-13 18:06:28 +0800

    新增全局路由守卫

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,22 @@ import { history } from "./shared/history";
 import "@svgstore";
 // 2. 引入组件样式
 import 'vant/lib/index.css';
+import { http } from "./shared/Http";
 const router = createRouter({ history, routes });
 
+
+const promise = http.get('/me')
+router.beforeEach(async (to, from) => {
+  if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start') {
+    return true
+  } else {
+    const path = await promise.then(
+      () => true,
+      () => '/sign_in?redirect=' + to.path
+    )
+    return path
+  }
+})
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
