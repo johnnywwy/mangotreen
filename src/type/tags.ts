380feac1a5b114c69e34803728060d1528131cd7
wuwenyu <johnnywwy@gmail.com>
2023-09-15 18:29:09 +0800

    加了点类型

--- src/type/tags.ts
+++ src/type/tags.ts
@@ -1,4 +1,11 @@
 
+
+export type tagsDTO = {
+  page: number,
+  kind: "expenses" | "income"
+}
+
+
 // 收入支出 标签类型
 export type Tag = {
   id: number;
@@ -16,4 +23,17 @@ export type Resources<T = any> = {
     per_page: number;
     count: number;
   }
+}
+
+export type Resource<T> = {
+  resources: T
+}
+
+export type Item = {
+  id: number
+  user_id: number
+  amount: number
+  tags_id: number[]
+  happen_at: string
+  kind: 'expenses' | 'income'
 }
