0593bd8678211cc213be251c4717382a8a58e7f7
johnnywwy <johnnywwy@gmail.com>
2023-09-24 22:54:41 +0800

    feat:Money组件新增 isShowDecimal 属性

--- src/shared/Money.tsx
+++ src/shared/Money.tsx
@@ -6,18 +6,26 @@ export const Money = defineComponent({
       type: Number as PropType<number>,
       required: true
     },
+    isShowDecimal: {
+      type: Boolean
+    }
   },
   setup: (props, content) => {
     const addZero = (n: number) => {
       const nStr = n.toString()
       const dotIndex = nStr.indexOf('.')
-      if (dotIndex < 0) {
-        return n + '.00'
-      } else if (nStr.substring(dotIndex).length === 2) {
-        return nStr + '0'
+      if (props.isShowDecimal === true) {
+        if (dotIndex < 0) {
+          return n + '.00'
+        } else if (nStr.substring(dotIndex).length === 2) {
+          return nStr + '0'
+        } else {
+          return nStr
+        }
       } else {
         return nStr
       }
+
     }
     return () => (<span>{addZero(props.value / 100)}</span>);
   },
