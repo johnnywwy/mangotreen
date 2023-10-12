a8df8dfed3d1977bd190ed239e1fc514a2ae51a1
wuwenyu <johnnywwy@gmail.com>
2023-10-12 11:48:40 +0800

    fix: 敬请期待页面添加返回按钮

--- src/shared/ComingSoon.tsx
+++ src/shared/ComingSoon.tsx
@@ -1,15 +1,24 @@
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { Button } from './Button'
 import { Center } from './Center'
 import s from './ComingSoon.module.scss'
 import { Icon } from './Icon'
 export const ComingSoon = defineComponent({
   setup: (props, context) => {
+    const router = useRouter()
+    const onClick = () => {
+      router.back()
+    }
     return () => (
       <div>
         <Center class={s.logo_wrapper}>
           <Icon name="logo" class={s.logo} />
         </Center>
         <p class={s.text}>敬请期待</p>
+        <p class={s.button} >
+          <Button onClick={onClick}>返回</Button>
+        </p>
       </div>
     )
   }
